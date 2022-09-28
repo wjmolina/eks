f312475a676ca7f8488b319ed438e9d096c57b58
Wil <molina.wilfredo.j@gmail.com>
2022-09-28 14:57:14 -0700

    vpc

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,27 +16,3 @@ terraform {
 provider "aws" {
   region = "us-west-1"
 }
-
-resource "aws_iam_role" "eks" {
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = {
-          Service = "eks.amazonaws.com"
-        }
-        Action = "sts:AssumeRole"
-      },
-    ]
-  })
-}
-
-data "aws_iam_policy" "AmazonEKSClusterPolicy" {
-  arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-}
-
-resource "aws_iam_role_policy_attachment" "eks_AmazonEKSClusterPolicy" {
-  role       = aws_iam_role.eks.name
-  policy_arn = data.aws_iam_policy.AmazonEKSClusterPolicy.arn
-}
