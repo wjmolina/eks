5b2c390b848afa0b9e85a01026b63de925c43c2b
Wil <molina.wilfredo.j@gmail.com>
2022-09-28 15:02:59 -0700

    use 3 public subnets instead

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,4 +1,4 @@
 resource "aws_cloudformation_stack" "vpc" {
   name         = "eks"
-  template_url = "https://s3.us-west-2.amazonaws.com/amazon-eks/cloudformation/2020-10-29/amazon-eks-vpc-private-subnets.yaml"
+  template_url = "https://s3.us-west-2.amazonaws.com/amazon-eks/cloudformation/2020-10-29/amazon-eks-vpc-sample.yaml"
 }
