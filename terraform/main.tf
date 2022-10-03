99545867f0f5f347431cc455969bcd1e747cf9aa
Wil Molina <molina.wilfredo.j@gmail.com>
2022-10-02 20:30:29 -0700

    Change Docker Hub to ECR (#1)
    * init
    
    * terraform workflow
    
    * test
    
    * test artifact
    
    * debug
    
    * try new docker
    
    * polish

--- terraform/main.tf
+++ terraform/main.tf
@@ -124,3 +124,20 @@ resource "aws_dynamodb_table" "eks" {
     type = "S"
   }
 }
+
+resource "aws_ecr_repository" "eks" {
+  name                 = "eks"
+  image_tag_mutability = "IMMUTABLE"
+  force_delete         = true
+  image_scanning_configuration {
+    scan_on_push = false
+  }
+}
+
+resource "local_file" "terraform_artifact" {
+  content  = <<EOT
+    EKS_NAME=${aws_eks_cluster.eks.name}
+    ECR_NAME=${aws_ecr_repository.eks.name}
+  EOT
+  filename = "terraform-artifact"
+}
