fac6abc9607617495b656d7977cce5597fc68447
wjmolina <molina.wilfredo.j@gmail.com>
2022-10-05 20:56:50 -0700

    test

--- app/app.py
+++ app/app.py
@@ -65,6 +65,12 @@ async def create_or_read_channel_singleton(id):
     }
 )
 async def create_milestone(ctx, date, *text):
+    """Create a milestone.
+
+    Args:
+        date (str): date!
+        text (str): text!
+    """
     singleton = await create_or_read_channel_singleton(milestones_channel_id)
     datetime.strptime(date, "%Y-%m-%d")
     milestones_table.put_item(
